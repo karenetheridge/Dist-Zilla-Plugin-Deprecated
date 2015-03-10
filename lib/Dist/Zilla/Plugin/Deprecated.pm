279be2254a3dd2e6f61b2258ccb609dee8e2024e
Karen Etheridge <ether@cpan.org>
2015-03-10 01:15:18 -0700

    fix vim modeline

--- lib/Dist/Zilla/Plugin/Deprecated.pm
+++ lib/Dist/Zilla/Plugin/Deprecated.pm
@@ -3,7 +3,7 @@ use warnings;
 package Dist::Zilla::Plugin::Deprecated;
 # ABSTRACT: add metadata to your distribution marking it as deprecated
 # KEYWORDS: plugin metadata module distribution deprecated
-# vim: set ts=8 sw=4 tw=78 et :
+# vim: set ts=8 sts=4 sw=4 tw=78 et :
 
 use Moose;
 with 'Dist::Zilla::Role::MetaProvider';
